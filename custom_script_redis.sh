1d84ecdaf345c9cb9d63489626126bcea51ed452
bhushangholve2006 <bhushangholve2006@gmail.com>
2018-01-10 17:42:31 +0530

    update

--- custom_script_redis.sh
+++ custom_script_redis.sh
@@ -9,7 +9,7 @@
 # install docker requirement
 #sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" xenial stable"
 sudo apt-get update
 
