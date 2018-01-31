27e5fa0542d4e27330dbb8653dd59d2f0a67f5a3
bhushangholve2006 <bhushangholve2006@gmail.com>
2018-01-31 11:06:00 +0530

    vmss tf

---
+++ test.tf
@@ -0,0 +1,71 @@
+resource "azurerm_virtual_machine_scale_set" "vmss" {
+  name                = "vmscaleset"
+  location            = "centralus"
+  resource_group_name = "chterrarg"
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name     = "Standard_DS1_v2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+    lun          = 0
+    caching        = "ReadWrite"
+    create_option  = "Empty"
+    disk_size_gb   = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "vmlab"
+    admin_username       = "azureuser"
+    admin_password       = "Passwword1234"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "IPConfiguration"
+      subnet_id                              = "/subscriptions/eb281cb6-3ead-40a8-9e9d-dc77f671516b/resourceGroups/chterrarg/providers/Microsoft.Network/virtualNetworks/fullstackgr-vnet/subnets/subnet1"
+      load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.bpepool.id}", ]
+    }
+  }
+
+  extension { 
+    name = "vmssextension"
+    publisher = "Microsoft.OSTCExtensions"
+    type = "CustomScriptForLinux"
+    type_handler_version = "1.2"
+    settings = <<SETTINGS
+    {
+        "commandToExecute": "sudo apt-get -y install nginx"
+    }
+    SETTINGS
+  }
+
+  tags {
+    environment = "codelab"
+  }
+}
